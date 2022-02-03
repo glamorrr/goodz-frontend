5fa9337a397f7a64318358b25cb8d135eaf2664a
glamorrr <anugrahtoni23@gmail.com>
2022-02-03 15:10:55 +0700

    chore: fix umami

--- src/utils/usePageviewAnalytics.js
+++ src/utils/usePageviewAnalytics.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 const usePageviewAnalytics = (page) => {
   useEffect(() => {
-    window.umami.trackView(page + window.location.search);
+    window.umami?.trackView(page + window.location.search);
   }, []);
 };
 
